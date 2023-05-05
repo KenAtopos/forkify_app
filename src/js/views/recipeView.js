cf2475ddce1b1b9bf4338487ab144c1d6b7d250c
Ken Atopos <z1158229761@outlook.com>
2023-05-05 14:49:17 +0800

    implement the servring function

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -14,6 +14,19 @@ class RecipeView extends View {
     );
   } // addHandlerRender is publisher, handler is the subscriber
 
+  addHandlerUpdateServings(handler) {
+    this._parentElement.addEventListener("click", function (event) {
+      const btn = event.target.closest(".btn--update-servings");
+
+      if (!btn) return;
+      console.log(btn);
+      const { updateTo } = btn.dataset;
+      if (+updateTo > 0) {
+        handler(+updateTo); // + here is used to convert the string to the number
+      }
+    });
+  }
+
   _generateMarkup() {
     return `
       <figure class="recipe__fig">
@@ -45,12 +58,16 @@ class RecipeView extends View {
           <span class="recipe__info-text">servings</span>
 
           <div class="recipe__info-buttons">
-            <button class="btn--tiny btn--increase-servings">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings - 1
+            }">
               <svg>
                 <use href="${icons}#icon-minus-circle"></use>
               </svg>
             </button>
-            <button class="btn--tiny btn--increase-servings">
+            <button class="btn--tiny btn--update-servings" data-update-to="${
+              this._data.servings + 1
+            }">
               <svg>
                 <use href="${icons}#icon-plus-circle"></use>
               </svg>
